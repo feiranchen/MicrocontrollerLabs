b8d59b9138b23680c8bbd1efab005b7cd815ffb1
carchard <carchard0@gmail.com>
2014-09-11 17:40:15 -0400

    added port, AOC, t0, and t1 initialization methods

--- lab1/lab1_4760.c
+++ lab1/lab1_4760.c
@@ -16,11 +16,12 @@
 #define begin {
 #define end }
 
+// Global variable declarations
+volatile char t0_count;    // counts the mS since last task1 call as updated by timer0
+volatile char ready;	// set to 1 every ~200mS
 
 int main(void) 
 begin
-
-	volatile char ready = 1;	// set to 1 every ~200mS
 	initialize();
 
 	while(1)
@@ -37,15 +38,82 @@ begin
 
 end
 
+/* Initialize all of the output ports
+portA - unused
+portB - start with B.3 as input and others as output
+portC - all output for LCD
+portD - unused
+*/
+void portInit(void)
+begin
+DDRA = 0xff;    // PORTA is unused and left to output low
+DDRB = 0xf7;    // sets B.3 to an input
+DDRC = 0xff;    // PORTC is used for the LCD and needs to output
+DDRD = 0xff;    // PORTD is unused and left to output low
 
+PORTA = 0x00;	// all of PORTA is output low
+PORTB = 0x08;   // B.3 has pull-up on. The rest of PORTB is output low
+PORTC = 0x00;   // PORTC is initialized to output low. LCD code will change
+PORTD = 0x00;   // PORTD is output low to save power
+end
+
+
+/* Initializes the AOC to remove the 1.23V reference 
+value, which will prepare for triggering timer1 appropriately
+*/
+void aocInit(void)
+begin
+
+ACSR = 0x07;     // sets the AOC to be on, using pin B.2, and rising on B.2 greater tan B.3
+
+end
+
+
+/* Initialize timer0 to be used for task1 timing. 
+This timer functions as the scheduler to update the LCD
+and to prompt the capacitance measurement. 
+
+Generates and ISR once per mS.
+*/
+void timer0Init(void)
+begin
+
+// zero variables to remove previous settings
+t0_count = 0;
+ready = 0;
+TCCR0A = 0x00;
+TCCR0B = 0x00;
+
+TCCR0A |= (1<<WGM1);    // sets the timer mode to compare and capture on OCR0A
+TCCR0B |= (1<<CS01)|(1<<CS02);    // Sets the prescaler for the timer to 64
+OCR0A = 250;	// sets the compare value to the timer counter
+TIMSK0 = 0x01;    // sets the OCR0A compare interrupt enable
+end
+
+
+/* Initialize timer1 to be used for measuring the rise time
+of the capacitor. This output compare will be captured on 
+the rising edge of the AOC
+*/
+void timer1Init(void)
+begin
+
+// zero variables to remove previous settings
+TCCR1A = 0x00;
+TCCR1B = 0x00;
+TIMSK1 = 0x00;
+
+TCCR1B |= (1<<ICES1)|(1<<WGM13)|(1<<WGM12)|(1<<CS10);    // sets the timer mode to compare and capture on ICR0 rising edge
+TIMSK1 |= (1<<ICIE1);
+end
 
 // initializes timers, pins, and compare registers
 void initialize(void)
 begin
-DDRB = ;	// set pin B.2 to be input and B.3 to be an output
-
-
-
+portInit();
+aocInit();
+timer0Init();
+timer1Init();
 end
 
 
@@ -68,14 +136,20 @@ end
 // used for measuring capacitance through discharge and comparison
 ISR(// insert timer0 compare vector here
 begin
-
-
+t0_count++;
+if t0_count == 200
+	begin
+	t0_count = 0;
+	ready = 1;
+	end
 end
 
 
 // used for tracking ~200mS to govern a new capacitance measurement
 ISR(// insert timer 1 compare vector here
 begin
-
+// note that the value of the timer is saved into: 
+// ICR1H
+// ICR1L
 
 end
