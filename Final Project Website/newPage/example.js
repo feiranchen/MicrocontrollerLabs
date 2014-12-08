9c2dce99a295baf93bb70663741493a8b579d95e
lalalah <fc254@cornell.edu>
2014-12-08 09:39:21 -0500

    removed long names

---
+++ Final Project Website/newPage/example.js
@@ -0,0 +1,188 @@
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+
+
+var serialPortName ="/dev/cu.usbserial-A603UZG1";
+var http = require('http');
+var url=require('url');
+var fs = require('fs');
+var filename = './samplePureVector';
+var querystring = require('querystring');
+var express = require('express');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(express.static(__dirname));
+app.set('port', process.env.PORT);
+// app.listen(app.get('port'), function(){
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/frame',function(req,res){
+  var frame=req.body.frame;
+  console.log("Frame = "+frame);
+  res.end("yes");
+  frame += "X-1Y-1D2*\n"
+  writeFile(frame);
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+function processPost(request, response, callback) {
+    var queryData = "";
+    if(typeof callback !== 'function') return null;
+
+    if(request.method == 'POST') {
+        request.on('data', function(data) {
+            queryData += data;
+            if(queryData.length > 1e6) {
+                queryData = "";
+                response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+                request.connection.destroy();
+            }
+        });
+
+        request.on('end', function() {
+            request.post = querystring.parse(queryData);
+            callback();
+        });
+
+    } else {
+        response.writeHead(405, {'Content-Type': 'text/plain'});
+        response.end();
+    }
+}
+
+
+// File parameters
+var frame_count= 0;
+var filename_base="/frame_";
+var sendCount = 0;
+var loaded = 0;
+var gerber = "";
+var gerber_lines= [];
+
+function writeFile(text){
+    fs.writeFile(__dirname+filename_base + frame_count, text, function(err) {
+	    if(err) {
+	        console.log(err);
+	    } else {
+	        console.log("The file was saved!"+frame_count);
+	    	frame_count++;
+	    	if(!loaded){
+	    	  readFile();
+	    	}
+	    }
+	}); 
+}
+
+function readFile() {
+	if (frame_count <= sendCount){
+		loaded = 0;
+		return;
+	}
+	fs.readFile(__dirname+filename_base + sendCount, 'utf8', function (err,data) {
+	  if (err) {
+	    return console.log(err);
+	  }else{
+		  if (sendCount<frame_count){
+		  	sendCount++;
+		  }
+		  gerber = data+"";
+		  gerber_lines = gerber.split('\n');
+		  console.log(gerber_lines);
+	  }
+	});
+}
+
+/*
+var SerialPort = require("serialport").SerialPort;
+var serialPort = 
+new SerialPort(serialPortName, {
+  baudrate: 9600
+});
+console.log('opened serialport');
+
+
+var inputBuffer = "";
+var line = "";
+var lineIndex = 0;
+
+function trimBuffer(){
+	// console.log('remaining' + inputBuffer + "<End>")
+	var lineEnding = inputBuffer.indexOf('\r\n');
+	if (lineEnding > -1){
+		line = inputBuffer.substring(0, lineEnding);
+		inputBuffer = inputBuffer.slice(lineEnding + 2)
+		console.log('got a line' + line + "<End>");
+		console.log('Buffer trimmed to' + inputBuffer + '<End>')
+	} else {
+		line = ""
+	}
+}
+
+readFile();
+serialPort.on("open", function () {
+	serialPort.on('data', function(data) {
+		console.log("Data!!"+data);
+		console.log("Printing buffer" +inputBuffer);
+		inputBuffer += data;
+		while(inputBuffer.indexOf('\r\n') > -1){
+			trimBuffer();
+			// This is to discard the noise charactors
+			line = line.substring(line.length-11)
+			
+			console.log('line is' + line + "<End>\nshouldbFile Length")
+			console.log(line.length + "length \n" + "File Length".length)
+			console.log(line == "File Length\r")
+			if (line == "File Length") {
+				console.log("frame" + frame_count + "read" + sendCount);
+				line = "";
+				console.log("File Length!!\n");
+				serialPort.write("{0}*\r\n".format(gerber_lines.length), function(err, results) {
+					serialPort.drain(function(){
+						console.log("Lenth Written:" + gerber_lines.length);
+						lineIndex = 0;
+						readFile();
+					});
+					//console.log('err ' + err);
+					//console.log('results ' + results);
+				});
+			} else if (line == "Hi") {
+				line = "";
+				if(gerber == "") {
+					console.log("File loading");
+					return;
+				}
+				//console.log('Received Hi\r\n');
+				serialPort.write(gerber_lines[lineIndex]+"*\r\n", function(err, results) {
+				//serialPort.write("test\r\n", function(err, results) {
+					serialPort.drain(function(){
+						console.log("Written."+ gerber_lines[lineIndex]+"\r\n");
+					});
+					lineIndex++;
+					console.log("sent"+lineIndex +": "+ gerber_lines[lineIndex]+"<End of MSG>");
+					//console.log('err ' + err);
+					//console.log('results ' + results);
+				});
+			}
+		}
+	});
+});*/
---
+++ Final Project Website/newPage/pad.js
@@ -0,0 +1,192 @@
+var drawing;
+var canvas, stage;
+var drawingCanvas;
+
+var oldPt;
+var oldMidPt;
+var title;
+var color;
+var stroke;
+var colors;
+var index;
+// params for straight line drawing
+var straight = 1;
+var straightCanvas;
+var tempPt;
+// params for outputing
+var strokes;
+var this_stroke;
+
+function gridX(x){
+    return Math.ceil(x +20);
+}
+function gridY(y){
+    return 500 - Math.ceil(y+20);
+}
+
+function handleDblclick(event){
+        drawing = 0;
+        console.log("doubleclicked!");
+    //this_stroke = this_stroke + "\n";
+    //strokes = strokes + (this_stroke.toString());
+    //console.log(strokes);
+    if (straight) {
+        var newPt;
+
+        if (Math.abs(oldPt.x - stage.mouseX) < Math.abs(oldPt.y - stage.mouseY) ){
+            newPt = new createjs.Point(oldPt.x, stage.mouseY);
+        } else {
+            newPt = new createjs.Point(stage.mouseX, oldPt.y);
+        }
+
+        if(Math.abs(oldPt.x - newPt.x) + Math.abs(oldPt.y - newPt.y)> 10){
+            this_stroke += "X"+gridX(newPt.x)+"Y"+gridY(newPt.y) + "D1*\n";
+            straightCanvas.setStrokeStyle(stroke, 'round', 'round').moveTo(oldPt.x, oldPt.y).beginStroke(color).lineTo(newPt.x, newPt.y).endStroke();
+            stage.update();
+        }
+        console.log(this_stroke);
+        strokes += this_stroke;
+
+        stage.removeChild(tempCanvas);
+        tempGraphics = new createjs.Graphics();
+        tempCanvas = new createjs.Shape(tempGraphics);
+        stage.update();
+    }
+    stage.removeEventListener("stagemousemove" , handleMouseMove);
+    stage.mouseMoveOutside = false;
+    stage.update();
+}
+
+function handleMouseDown(event) {
+    if (stage.contains(title)) { stage.clear(); stage.removeChild(title); }
+    // color = colors[(index++)%colors.length];
+    // stroke = Math.random()*30 + 10 | 0;
+    color = "#828b20";
+    stroke = 10;
+    if(drawing ==0){
+        console.log("handleMouseDown seting old point");
+        oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+        tempPt = oldPt;
+        this_stroke = "X"+gridX(stage.mouseX)+"Y"+gridY(stage.mouseY) + "D2*\n";
+        // console.log(this_stroke+" start");
+        stage.addChild(tempCanvas);
+        oldMidPt = oldPt;
+    }
+    drawing = 1;
+    stage.addEventListener("stagemousemove" , handleMouseMove);
+}
+
+function handleMouseMove(event) {
+    if (straight) {
+        // console.log("start"+ oldPt.x + " "+oldPt.y);
+        // console.log("now"+ tempPt.x + " "+tempPt.y);
+        var old_tempPt = new createjs.Point(tempPt.x, tempPt.y);
+        if (Math.abs(oldPt.x - stage.mouseX) < Math.abs(oldPt.y - stage.mouseY) ){
+            tempPt = new createjs.Point(oldPt.x, stage.mouseY);
+        } else {
+            tempPt = new createjs.Point(stage.mouseX, oldPt.y);
+        }
+
+        // console.log("start2"+ oldPt.x + " "+oldPt.y);
+        tempGraphics.clear().setStrokeStyle(stroke+1, 'round', 'round').moveTo(oldPt.x, oldPt.y).beginStroke("#333").lineTo(old_tempPt.x, old_tempPt.y).endStroke();
+        stage.update();
+
+        tempGraphics.clear().setStrokeStyle(stroke, 'round', 'round').moveTo(oldPt.x, oldPt.y).beginStroke(color).lineTo(tempPt.x, tempPt.y).endStroke();
+        stage.update();
+        return;
+    }
+    var midPt = new createjs.Point(oldPt.x + stage.mouseX>>1, oldPt.y+stage.mouseY>>1);
+
+    drawingCanvas.graphics.clear().setStrokeStyle(stroke, 'round', 'round').beginStroke(color).moveTo(midPt.x, midPt.y).curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+
+    oldPt.x = stage.mouseX;
+    oldPt.y = stage.mouseY;
+    if(drawing == 1) 
+        {this_stroke = this_stroke + "x"+stage.mouseX+"y"+stage.mouseY + "; ";}
+    // console.log("here"+drawing);
+
+    oldMidPt.x = midPt.x;
+    oldMidPt.y = midPt.y;
+
+    stage.update();
+}
+
+function handleMouseUp(event) {
+    //this_stroke = this_stroke + "\n";
+    //strokes = strokes + (this_stroke.toString());
+    //console.log(strokes);
+    if (straight) {
+        var newPt;
+
+        if (Math.abs(oldPt.x - stage.mouseX) < Math.abs(oldPt.y - stage.mouseY) ){
+            newPt = new createjs.Point(oldPt.x, stage.mouseY);
+        } else {
+            newPt = new createjs.Point(stage.mouseX, oldPt.y);
+        }
+
+        if(Math.abs(oldPt.x - newPt.x) + Math.abs(oldPt.y - newPt.y)> 10){
+            this_stroke = this_stroke + "X"+gridX(newPt.x)+"Y"+gridY(newPt.y) + "D1*\n";
+            straightCanvas.setStrokeStyle(stroke, 'round', 'round').moveTo(oldPt.x, oldPt.y).beginStroke(color).lineTo(newPt.x, newPt.y).endStroke();
+            stage.update();
+        }
+
+        // console.log("start"+ oldPt.x + " "+oldPt.y);
+        // console.log("end"+ newPt.x + " "+newPt.y);
+        stage.removeChild(tempCanvas);
+        tempGraphics = new createjs.Graphics();
+        tempCanvas = new createjs.Shape(tempGraphics);
+        stage.update();
+
+
+        oldPt = newPt;
+        tempPt = oldPt;
+        //this_stroke = "X"+gridX(stage.mouseX)+"Y"+gridY(stage.mouseY) + "D2*\n";
+        // console.log(this_stroke+" start");
+        stage.addChild(tempCanvas);
+        oldMidPt = oldPt;
+    }
+    // stage.removeEventListener("stagemousemove" , handleMouseMove);
+    stage.update();
+}
+
+function init() {
+
+    if (window.top != window) {
+        document.getElementById("header").style.display = "none";
+    }
+    canvas = document.getElementById("myCanvas");
+    index = 0;
+    colors = ["#828b20", "#b0ac31", "#cbc53d", "#fad779", "#f9e4ad", "#faf2db", "#563512", "#9b4a0b", "#d36600", "#fe8a00", "#f9a71f"];
+    strokes = "";
+    drawing = 0;
+    // console.log("start"+drawing);
+    //check to see if we are running in a browser with touch support
+    stage = new createjs.Stage(canvas);
+    stage.autoClear = false;
+    stage.enableDOMEvents(true);
+
+    createjs.Touch.enable(stage);
+    createjs.Ticker.setFPS(24);
+
+    
+    tempGraphics = new createjs.Graphics();
+    tempCanvas = new createjs.Shape(tempGraphics);
+    straightCanvas = new createjs.Graphics();
+    drawingCanvas = new createjs.Shape(straightCanvas);
+
+    stage.addEventListener("stagemousedown", handleMouseDown);
+    stage.addEventListener("stagemouseup", handleMouseUp);
+    canvas.addEventListener("dblclick", handleDblclick);
+
+    title = new createjs.Text("Click and Drag\n    to draw", "36px Arial", "#777777");
+    title.x = 100;
+    title.y = 200;
+    stage.addChild(title);
+
+    stage.addChild(drawingCanvas);
+    stage.update();
+}
+
+function stop() {}
+
+
